c307fa0e71df9644eecaeab3144330896d988a70
Daniel Johnson <dan@balancerockmedia.com>
2013-08-06 18:00:31 -0400

    edits

--- session3/actions.php
+++ session3/actions.php
@@ -1,10 +1,5 @@
 <?php
 
-define('HOST', '127.0.0.1');
-define('PORT', null);
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('DATABASE', 'cdia_restaurant');
 define('API_URL', 'http://127.0.0.1/~Dan/cdiabu/object-oriented-programming/session3/api/index.php/');
 // define('API_URL', 'http://166.78.112.120/cdiabu/index.php/');
 
